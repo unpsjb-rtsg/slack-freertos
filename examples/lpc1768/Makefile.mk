6207b4efd3d111f93cf0ad007d1817afc76f6b96
fepz <franpaez+github@gmail.com>
2020-07-19 22:11:53 -0300

    Wrap functions only when using FreeRTOS v10.x
    Do not add the wrap flag when building binaries with a FreeRTOS version older than 10.

--- examples/lpc1768/Makefile.mk
+++ examples/lpc1768/Makefile.mk
@@ -110,7 +110,12 @@ LD_FLAGS = $(CPU) -Wl,--gc-sections -u _printf_float -u _scanf_float
 LD_SYS_LIBS = -lstdc++ -lsupc++ -lm -lc -lgcc -lnosys
 
 # Replace these functions
+ifeq ($(FREERTOS_KERNEL_VERSION_NUMBER), v10.3.1)
 WRAP = -Wl,--wrap=vTaskDelayUntil -Wl,--wrap=xTaskIncrementTick
+endif
+ifeq ($(FREERTOS_KERNEL_VERSION_NUMBER), v10.2.1)
+WRAP = -Wl,--wrap=vTaskDelayUntil -Wl,--wrap=xTaskIncrementTick
+endif
 
 export CPU CC_SYMBOLS MBED_INCLUDE_PATHS
 
