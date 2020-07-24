19a8ca135199f4390c4352186af45e246a34a2e8
fepz <franpaez+github@gmail.com>
2020-07-24 18:36:53 -0300

    Add Tracealyzer include paths.

--- examples/edu-ciaa-nxp/Makefile.mk
+++ examples/edu-ciaa-nxp/Makefile.mk
@@ -74,6 +74,11 @@ ifeq ($(TZ), 1)
     INCLUDE_PATHS += -I../../libs/Tracealizer/$(TRACEALIZER_VERSION_NUMBER)/config
     CC_FLAGS += -DTRACEALYZER_v3_1_3
   endif
+  ifeq ($(TRACEALIZER_VERSION_NUMBER), v3.3.1)
+    INCLUDE_PATHS += -I../../libs/Tracealizer/$(TRACEALIZER_VERSION_NUMBER)/include
+    INCLUDE_PATHS += -I../../libs/Tracealizer/$(TRACEALIZER_VERSION_NUMBER)/config
+    CC_SYMBOLS += -DTRACEALYZER_v3_3_1
+  endif
 endif
 
 ###############################################################################
