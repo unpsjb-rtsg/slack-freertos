36ec7abfac6111a1bede195ebe1645311b0981e3
Fran <franpaez+github@gmail.com>
2021-01-18 09:12:35 -0300

    Change include.

--- slack/v10.4.1/slack_algorithms/slack_algorithms.h
+++ slack/v10.4.1/slack_algorithms/slack_algorithms.h
@@ -5,12 +5,12 @@
  *      Author: Francisco E. Páez
  */
 
-#ifndef SLACK_V10_3_1_SLACK_ALGORITHMS_SLACK_ALGORITHMS_H_
-#define SLACK_V10_3_1_SLACK_ALGORITHMS_SLACK_ALGORITHMS_H_
+#ifndef SLACK_V10_4_1_SLACK_ALGORITHMS_SLACK_ALGORITHMS_H_
+#define SLACK_V10_4_1_SLACK_ALGORITHMS_SLACK_ALGORITHMS_H_
 
 #include "FreeRTOS.h"
 
 void vTaskCalculateSlack_alg( TaskHandle_t xTask, const TickType_t xTc,
         const List_t * pxTasksList );
 
-#endif /* SLACK_V10_3_1_SLACK_ALGORITHMS_SLACK_ALGORITHMS_H_ */
+#endif /* SLACK_V10_4_1_SLACK_ALGORITHMS_SLACK_ALGORITHMS_H_ */
