114ffdb10ca058a9227242bffafcb06fcdf44a97
Francisco E. Paez <franpaez+github@gmail.com>
2021-11-25 15:15:57 -0300

    Remove streaming code from FRDM-K64F examples.
    The FRDM-K64F examples were compiled with the streaming functionality of
    Tracealyzer, which was not used.

--- libs/FreeRTOS/Makefile.mk
+++ libs/FreeRTOS/Makefile.mk
@@ -98,16 +98,9 @@ ifeq ($(TZ), 1)
   ifeq ($(TRACEALIZER_VERSION_NUMBER), v3.3.1)
     OBJECTS += ../Tracealizer/$(TRACEALIZER_VERSION_NUMBER)/trcKernelPort.o
     OBJECTS += ../Tracealizer/$(TRACEALIZER_VERSION_NUMBER)/trcSnapshotRecorder.o
-    OBJECTS += ../Tracealizer/$(TRACEALIZER_VERSION_NUMBER)/trcStreamingRecorder.o
     
     INCLUDE_PATHS += -I../Tracealizer/$(TRACEALIZER_VERSION_NUMBER)/config
     INCLUDE_PATHS += -I../Tracealizer/$(TRACEALIZER_VERSION_NUMBER)/include
-    
-    ifeq ($(TARGET), frdm-k64f)      
-      OBJECTS += ../Tracealizer/$(TRACEALIZER_VERSION_NUMBER)/streamports/Jlink_RTT/SEGGER_RTT.o
-      INCLUDE_PATHS += -I../Tracealizer/$(TRACEALIZER_VERSION_NUMBER)/streamports/Jlink_RTT/include
-    endif
-    
     CC_SYMBOLS += -DTRACEALYZER_v3_3_1
   endif
   
